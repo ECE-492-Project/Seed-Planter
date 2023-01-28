d63b5ce7e3fb2eb1fafd6fbeb2b5a772a5069f4f
w3ichen <w3ichen@users.noreply.github.com>
2023-01-28 23:34:33 +0000

    deploy: f6e9804a7fc5c835e0ee23570613bc106c349f65

--- _next/static/kCWllZSuo6ihO6mnRlTgI/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/660-774dd06cd9132d27.js","static/chunks/pages/index-b5d7cdf2f3e827bf.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
