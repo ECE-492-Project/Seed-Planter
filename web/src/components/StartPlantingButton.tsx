e5c5b237b7e5bc9009e4c297c85d534e37072637
Weichen Qiu <57570491+w3ichen@users.noreply.github.com>
2023-03-17 18:14:19 -0600

    Bluetooth changes

--- web/src/components/StartPlantingButton.tsx
+++ web/src/components/StartPlantingButton.tsx
@@ -3,18 +3,57 @@ import { PlayCircleOutline } from "@mui/icons-material";
 import { Button, ButtonProps, CircularProgress } from "@mui/material";
 import { computePath } from "./geo";
 
+const handleCharacteristicValueChanged = (event: any) => {
+  console.log(event.target.value.getUint8(0) + "%");
+};
+const onDisconnected = (event: any) => {
+  const device = event.target;
+  alert(`Device ${device.name} is disconnected.`);
+};
+
 /**
  * Web Bluetooth API: https://developer.chrome.com/articles/bluetooth/
  * https://developer.mozilla.org/en-US/docs/Web/API/Bluetooth/requestDevice
+ * Tutorial: https://rebeccamdeprey.com/blog/interact-with-bluetooth-devices-using-the-web-bluetooth-api
  */
 const connectToRobot = async () => {
   try {
     const device = await (navigator as any).bluetooth.requestDevice({
       // filters: [{ name: "raspberrypi" }], // filter by device name
       acceptAllDevices: true, // accept all devices
-      optionalServices: ["battery_service"], // Required to access service later.
+      // optionalServices: ["battery_service"], // Required to access service later.
+      // filters: [{ services: ["battery_service"] }],
     });
-    console.log("DEVICE:", device);
+
+    // On disconnect events
+    device.addEventListener("gattserverdisconnected", onDisconnected);
+    window.onbeforeunload = function () {
+      // On page leave, disconnect
+      device.gatt.disconnect();
+    };
+
+    console.log("Device:", device);
+    const server = await device.gatt.connect();
+    console.log("Server:", server);
+    const service = await server.getPrimaryService("battery_service");
+    console.log("Service:", service);
+    const characteristic = await service.getCharacteristic("battery_level");
+    console.log("Characteristic:", characteristic);
+
+    // Reading the value
+    const reading = await characteristic.readValue();
+    console.log(reading.getUint8(0) + "%");
+
+    // Notification
+    characteristic.startNotifications();
+    characteristic.addEventListener(
+      "characteristicvaluechanged",
+      handleCharacteristicValueChanged
+    );
+
+    // Writing value
+    const resetEnergyExpended = Uint8Array.of(1);
+    return characteristic.writeValue(resetEnergyExpended);
   } catch (e) {
     console.error(e);
   }
