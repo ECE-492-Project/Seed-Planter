01d64e2ee37168902dabff7b80e020a5c580b4fe
Weichen Qiu <57570491+w3ichen@users.noreply.github.com>
2023-03-20 23:11:22 -0600

    Is connecting to gatt server

--- web/src/components/StartPlantingButton.tsx
+++ web/src/components/StartPlantingButton.tsx
@@ -4,7 +4,7 @@ import { Button, ButtonProps, CircularProgress } from "@mui/material";
 import { computePath } from "./geo";
 
 const handleCharacteristicValueChanged = (event: any) => {
-  console.log(event.target.value.getUint8(0) + "%");
+  console.log("CHANGED:", event.target.value.getUint8(0) + "%");
 };
 const onDisconnected = (event: any) => {
   const device = event.target;
@@ -17,12 +17,22 @@ const onDisconnected = (event: any) => {
  * Tutorial: https://rebeccamdeprey.com/blog/interact-with-bluetooth-devices-using-the-web-bluetooth-api
  */
 const connectToRobot = async () => {
+  // IDs defined on raspberry pi
+  const SERVICE_UUID = "6e400001-b5a3-f393-e0a9-e50e24dcca9e";
+  const RX_CHARACTERISTIC_UUID = "6e400002-b5a3-f393-e0a9-e50e24dcca9e";
+  const TX_CHARACTERISTIC_UUID = "6e400003-b5a3-f393-e0a9-e50e24dcca9e";
+
   try {
     const device = await (navigator as any).bluetooth.requestDevice({
-      filters: [{ name: "SPR" }], // filter by device name
+      filters: [
+        {
+          name: "SPR",
+          id: "TDh83w3Qnb1C+KOkhyvMUw==",
+        },
+      ], // filter by device name
       // acceptAllDevices: true, // accept all devices
-      // optionalServices: ["org.bluez.GattService1"], // Required to access service later.
-      // filters: [{ name: ["rpi-gatt-server"] }],
+      optionalServices: [SERVICE_UUID], // Required to access service later.
+      // filters: [{ services: ["rpi-gatt-server"] }],
     });
 
     // On disconnect events
@@ -35,35 +45,43 @@ const connectToRobot = async () => {
     console.log("Device:", device);
     const server = await device.gatt.connect();
     console.log("Server:", server);
-    const service = await server.getPrimaryService("battery_service");
+    const service = await server.getPrimaryService(SERVICE_UUID);
     console.log("Service:", service);
-    const characteristic = await service.getCharacteristic("battery_level");
-    console.log("Characteristic:", characteristic);
+    const rxCharacteristic = await service.getCharacteristic(
+      RX_CHARACTERISTIC_UUID
+    );
+    console.log("rxCharacteristic:", rxCharacteristic);
+    const txCharacteristic = await service.getCharacteristic(
+      TX_CHARACTERISTIC_UUID
+    );
+    console.log("txCharacteristic:", txCharacteristic);
 
     // Reading the value
-    const reading = await characteristic.readValue();
-    console.log(reading.getUint8(0) + "%");
+    const reading = await rxCharacteristic.readValue();
+    console.log("READ:", reading.getUint8(0) + "%");
 
-    // Notification
-    characteristic.startNotifications();
-    characteristic.addEventListener(
+    // Notification on receiver
+    rxCharacteristic.startNotifications();
+    rxCharacteristic.addEventListener(
       "characteristicvaluechanged",
       handleCharacteristicValueChanged
     );
 
-    // Writing value
-    const resetEnergyExpended = Uint8Array.of(1);
-    return characteristic.writeValue(resetEnergyExpended);
+    return txCharacteristic;
   } catch (e) {
     console.error(e);
+    return null;
   }
 };
 /**
  * Transmit data to the robot
  */
-const transmitData = (data: any) => {
+const transmitData = async (data: any) => {
   console.log("Transmitting data:", data);
-  connectToRobot();
+  const tx = await connectToRobot();
+  // Writing value to transmitter
+  const resetEnergyExpended = Uint8Array.of(1);
+  tx.writeValue(resetEnergyExpended);
 };
 
 export const handleSubmit =
@@ -73,7 +91,7 @@ export const handleSubmit =
     myPosition,
     setPath,
   }: Props & { setLoading: (loading: boolean) => void }) =>
-  (e: any) => {
+  async (e: any) => {
     e.preventDefault(); // prevent page reload
     setLoading(true);
     console.log("Coords received:", coords);
@@ -84,7 +102,7 @@ export const handleSubmit =
     // Draw path on map
     setPath(pathCoords);
 
-    transmitData({});
+    await transmitData(pathCoords);
 
     setLoading(false);
   };
